55ca27b53b02f702869506449a6c2b9f4d3a9e52
Cao Quan <quancao08@gmail.com>
2024-07-12 08:51:50 +0700

    update lai post

---
+++ PRN221/PRN221/Pages/BlogPages/PostDetail.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PRN221.Pages.BlogPages
+{
+    public class PostDetailModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
