8b29acc81ba5a482414f5043c5b04450b8955c82
Cao Quan <quancao08@gmail.com>
2024-07-12 07:25:13 +0700

    update lai viewallblog

--- PRN221/Repository/Repository/BlogRepository.cs
+++ PRN221/Repository/Repository/BlogRepository.cs
@@ -39,7 +39,7 @@ namespace Repository.Repository
 
         public async Task<List<Blog>> GetAll()
         {
-            List<Blog> list = await _context.Blogs.ToListAsync();
+            List<Blog> list = await _context.Blogs.Include(x=> x.User).ToListAsync();
             return list;
         }
 
