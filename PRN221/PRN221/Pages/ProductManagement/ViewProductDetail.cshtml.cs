a226f43fd7ba6638e19ea8d8da238301ad594df6
Cao Quan <quancao08@gmail.com>
2024-07-21 21:46:28 +0700

    create a view product detail for user

---
+++ PRN221/PRN221/Pages/ProductManagement/ViewProductDetail.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PRN221.Pages.ProductManagement
+{
+    public class ViewProductDetailModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
